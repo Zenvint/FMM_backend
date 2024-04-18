a317754bac9d56052ac2f4ec52069513860530e6
bungongtoto <kingsley.bungong@ictuniversity.edu.cm>
2024-04-18 21:59:03 +0100

    added class route and controller

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ app.use('/', express.static(path.join(__dirname, 'public')))
 app.use('/', require('./routes/root'))
 app.use('/users', require('./routes/userRoutes'))
 app.use('/sections', require('./routes/sectionRoutes'))
+app.use('/classes', require('./routes/classRoutes'))
 
 app.all('*', (req, res) => {
     res.status(404)
