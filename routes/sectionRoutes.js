0409442c3e9a4866b065aa6cba8f57a775e9d217
bungongtoto <kingsley.bungong@ictuniversity.edu.cm>
2024-04-22 10:22:32 +0100

    completed the authentification

--- routes/sectionRoutes.js
+++ routes/sectionRoutes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const sectionsController = require('../controllers/sectionsController')
+const verifyJWT = require('../middleware/verifyJWT')
 
+router.use(verifyJWT)
 router.route("/")
     .get(sectionsController.getAllSections)
     .post(sectionsController.createNewSection)
