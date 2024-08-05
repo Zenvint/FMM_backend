5e03347f1dd85802c4ac810ad8b2d0d3551a8d5b
bungongtoto <kingsley.bungong@ictuniversity.edu.cm>
2024-08-05 16:57:10 +0100

    fee history and salary histroy

--- routes/salaryRoutes.js
+++ routes/salaryRoutes.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const salariesController = require('../controllers/salariesController')
-const verifyJWT = require('../middleware/verifyJWT')
+const salariesController = require("../controllers/salariesController");
+const verifyJWT = require("../middleware/verifyJWT");
 
-router.use(verifyJWT)
-router.route("/")
-    .get(salariesController.getAllSalaries)
-    .patch(salariesController.updateSalary)
+router.use(verifyJWT);
+router
+  .route("/")
+  .get(salariesController.getAllSalaries)
+  .patch(salariesController.updateSalary);
+
+router.route("/history").patch(salariesController.updateHistory);
+
+router.route("/newsalarymonth").post(salariesController.startNewSalaryMonth);
 
 module.exports = router;
